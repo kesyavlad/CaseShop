4b8c2f7c64a588af8cf9713c26727aefbf1b29d3
Kesya <kesyavlad74@gmail.com>
2024-07-18 15:04:26 +0300

    add feedback

---
+++ src/components/Reviews.tsx
@@ -0,0 +1,7 @@
+import MaxWidthWrapper from "./MaxWidthWrapper";
+
+export function Reviews() {
+  <MaxWidthWrapper className="relative max-w-5xl">
+    <img aria-hidden="true" src="/what-people-are-buying.png" />
+  </MaxWidthWrapper>;
+}
