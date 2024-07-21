00786d012abfab494deb15a532c4ceac31ad3973
Kesya <kesyavlad74@gmail.com>
2024-07-21 16:24:11 +0300

    add posible create custom case

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,6 +1,33 @@
-const Page = async () => {
-  // make db request\
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignConfiguration from "./DesignConfiguration";
 
-  return <p></p>;
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+  if (!configuration) {
+    return notFound();
+  }
+  const { imageUrl, width, height } = configuration;
+  return (
+    <DesignConfiguration
+      configId={configuration.id}
+      imageDimensions={{ width, height }}
+      imageUrl={imageUrl}
+    />
+  );
 };
 export default Page;
